b029d72da0b8d77e011d4eae531134d8a48336ba
BibinBennyPeter <bibinbennypeter@gmail.com>
2023-05-14 14:47:48 +0530

    patient check

--- frontend/src/pages/Patient.jsx
+++ frontend/src/pages/Patient.jsx
@@ -13,16 +13,17 @@ const Patient = (props) => {
   const [Veiw, setVeiw] = useState(false);
   const [Request, setRequest] = useState([]);
   const [Record, setRecord] = useState([]);
-  const [Pop, setPop] = useState(true);
+  const [Details, setDetails] = useState();
+  const [Pop, setPop] = useState(false);
   // const location = useLocation();
   const tk = sessionStorage.getItem("tk");
   const [cnt, setCnt] = useState(0);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchDetails = async () => {
       try {
         const response = await fetch(
-          "http://localhost:8000/api/request/mint/check",
+          "http://localhost:8000/api/details/check",
           {
             method: "GET",
             headers: {
@@ -33,22 +34,22 @@ const Patient = (props) => {
         );
 
         if (response.ok) {
-          const data = await response.json();
-          console.log(data);
-          console.log(data._id);
-          setRequest(data);
+          const details = await response.json();
+          console.log(details);
+          console.log(details._id);
+          setDetails(details);
         } else {
           throw new Error("Request failed with status: " + response.status);
         }
       } catch (error) {
+        if (error == 400) {
+          setPop(true);
+        }
         console.log("Error: " + error.message);
       }
     };
-    fetchData();
-    console.log(cnt);
-  }, [cnt]);
+    fetchDetails();
 
-  useEffect(() => {
     const fetchHistory = async () => {
       try {
         const response = await fetch("http://localhost:8000/api/user/history", {
@@ -73,6 +74,36 @@ const Patient = (props) => {
     };
     fetchHistory();
   }, []);
+
+  useEffect(() => {
+    const fetchReq = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:8000/api/request/mint/check",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${tk}`,
+            },
+          }
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+          console.log(data._id);
+          setRequest(data);
+        } else {
+          throw new Error("Request failed with status: " + response.status);
+        }
+      } catch (error) {
+        console.log("Error: " + error.message);
+      }
+    };
+    fetchReq();
+    console.log(cnt);
+  }, [cnt]);
 
   // const { fileBase64String } = location.state;
   // console.log(fileBase64String);
